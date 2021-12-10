33686f7cbe147f7915061aa56780ef1f8db7f7f1
Shane <mecham.shane@gmail.com>
2021-12-10 14:03:05 -0700

    Removed density as the current algorithm for getting it to work introduced more problems than it solved.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::{core::FixedTimestep, prelude::*};
+use bevy::{core::FixedTimestep,
+           prelude::*,
+};
+
 // Game State resource
 pub struct GameState {
     pub game_paused: bool,
@@ -36,10 +39,27 @@ pub enum MyStage {
     BeforeMove,
     AfterMove,
 }
+// CONSTANT variables
 const GRID_HEIGHT: usize = 30;
-const GRID_WIDTH: usize = 30;
+const GRID_WIDTH: usize = 60;
 const GRID_STEP: usize = 30;
-fn startup_system(mut commands: Commands, mut grid: ResMut<Grid>) {
+fn keyboard_system(
+    mut game_state: ResMut<GameState>,
+    keyboard_input: Res<Input<KeyCode>>
+) {
+    if keyboard_input.pressed(KeyCode::Key1) {
+        game_state.selected_particle = 0;
+    } else if keyboard_input.pressed(KeyCode::Key2) {
+        game_state.selected_particle = 1;
+    } else if keyboard_input.pressed(KeyCode::Key3) {
+        game_state.selected_particle = 2;
+    } else if keyboard_input.pressed(KeyCode::Key4) {
+        game_state.selected_particle = 3;
+    }
+}
+
+fn startup_system(mut commands: Commands, mut grid: ResMut<Grid>,
+) {
     // let mut camera = OrthographicCameraBundle::new_2d();
     // let screenX = (GRID_HEIGHT as f32 * GRID_STEP as f32) / 2 as f32;
     // let screenY = (GRID_WIDTH as f32 * GRID_STEP as f32) / 2 as f32;
@@ -59,7 +79,7 @@ fn startup_system(mut commands: Commands, mut grid: ResMut<Grid>) {
         grid_step: step,
         selected_particle: 2,
     });
-    commands.insert_resource(ParticleList(vec![
+    let particle_list = vec![
         ParticleData {
             density: 1.0,
             particle_type: ParticleType::Liquid,
@@ -73,17 +93,19 @@ fn startup_system(mut commands: Commands, mut grid: ResMut<Grid>) {
         ParticleData {
             density: 2.6,
             particle_type: ParticleType::Solid,
-            material: Color::rgb(0.4, 0.2, 0.8),
+            material: Color::DARK_GRAY,
         },
         ParticleData {
             density: 0.00059,
             particle_type: ParticleType::Gas,
             material: Color::ALICE_BLUE,
         },
-    ]));
+    ];
+
+    commands.insert_resource(ParticleList(particle_list));
     grid.0.append(&mut vec![
-        vec![0.0; grid_height as usize];
-        grid_width as usize
+        vec![0.0; GRID_HEIGHT + 1];
+        GRID_WIDTH + 1
     ])
 }
 
@@ -105,8 +127,8 @@ fn calc_next_system(
                 Vec2::new(0.0, -1.0),
                 Vec2::new(-1.0, -1.0),
                 Vec2::new(1.0, -1.0),
-                Vec2::new(1.0, 0.0),
                 Vec2::new(-1.0, 0.0),
+                Vec2::new(1.0, 0.0),
             ],
             ParticleType::Gas => vec![
                 Vec2::new(0.0, 1.0),
@@ -116,54 +138,49 @@ fn calc_next_system(
                 Vec2::new(1.0, 0.0),
             ],
         };
+        let mut changed = false;
         for loc in v.iter() {
             let modified_loc = location + *loc;
             let valid_check = !(modified_loc.x < 0.0)
-                && !(modified_loc.x >= game_state.grid_width)
+                && !(modified_loc.x > game_state.grid_width)
                 && !(modified_loc.y < 0.0)
-                && !(modified_loc.y >= game_state.grid_height);
+                && !(modified_loc.y > game_state.grid_height);
             if valid_check {
-                if grid.0[modified_loc.x as usize][modified_loc.y as usize] == 0.0
-                    || grid.0[modified_loc.x as usize][modified_loc.y as usize]
-                        > particle_data.density
+                let neighbor_particle_data = grid.0[modified_loc.x as usize][modified_loc.y as usize].clone();
+                if neighbor_particle_data == 0.0
                 {
+                    changed = true;
                     particle_locations.new_location = modified_loc;
-                    grid.0[modified_loc.x as usize][modified_loc.y as usize] =
-                        particle_data.density;
+                    grid.0[modified_loc.x as usize][modified_loc.y as usize] = particle_data.density;
                     grid.0[location.x as usize][location.y as usize] = 0.0;
                     break;
                 }
             }
         }
+        if !changed {
+            particle_locations.new_location = location;
+        }
     }
 }
 
 fn move_next_system(
-    mut query: Query<(&mut ParticleLocations, &mut Transform)>,
+    mut query: Query<(&mut ParticleLocations, &mut Transform), Changed<ParticleLocations>>,
     game_state: Res<GameState>,
 ) {
     let step = game_state.grid_step;
     for (mut particle_locations, mut transform) in query.iter_mut() {
         let grid_x = game_state.grid_width / 2.0;
         let grid_y = game_state.grid_height / 2.0;
-        let bound_x = grid_x * step;
-        let bound_y = grid_y * step;
-        let mut changed_location =
+
+        let changed_location =
             (particle_locations.new_location - Vec2::new(grid_x, grid_y)) * Vec2::splat(step);
-        if changed_location.x <= -bound_x {
-            changed_location.x = -bound_x + step;
-        } else if changed_location.x >= bound_x {
-            changed_location.x = bound_x - step;
-        }
-        if changed_location.y <= -bound_y {
-            changed_location.y = -bound_y + step;
-        } else if changed_location.y >= bound_y {
-            changed_location.y = bound_y + step;
-        }
+
         transform.translation = changed_location.extend(0.0);
         particle_locations.old_location = particle_locations.new_location;
     }
+
 }
+
 
 fn round(value: f32) -> f32 {
     let floor = value.floor();
@@ -172,25 +189,24 @@ fn round(value: f32) -> f32 {
     return if value >= mean { ceil } else { floor };
 }
 
-fn mouse_to_grid(coord: Vec2, step: f32, cellsX: f32, cellsY: f32) -> Vec2 {
-    let x_value = round(coord.x / step);
-    let y_value = round(coord.y / step);
-    let grid_x = cellsX * step;
-    let grid_y = cellsY * step;
-    let board_x = grid_x / 2.0;
-    let board_y = grid_y / 2.0;
-    let mut x = x_value * step - board_x;
-    let mut y = y_value * step - board_y;
-
+fn mouse_to_grid(coord: Vec2, step: f32, cells_x: f32, cells_y: f32) -> Vec2 {
+    let x_value = round(coord.x / step - 1.0);
+    let y_value = round(coord.y / step - 1.);
+    let grid_x = cells_x / 2.0;
+    let grid_y = cells_y / 2.0;
+    let mut x = (x_value - grid_x) * step;
+    let mut y = (y_value - grid_y) * step;
+    let board_x = grid_x * step;
+    let board_y = grid_y * step;
     if x >= board_x {
-        x = board_x - step;
+        x = board_x;
     } else if x <= -board_x {
-        x = -board_x + step;
+        x = -board_x;
     }
-    if y >= board_y {
-        y = board_y - step;
+    if y >=board_y {
+        y = board_y;
     } else if y <= -board_y {
-        y = -board_y + step;
+        y = -board_y;
     }
 
     Vec2::new(x, y)
@@ -217,31 +233,30 @@ pub fn mouse_click_system(
     if mouse_button_input.pressed(MouseButton::Left) {
         let particle_data = particle_list.0[game_state.selected_particle];
 
-        for i in 0..game_state.brush_size {
-            let translation = mouse_to_grid(
-                win,
-                game_state.grid_step,
-                game_state.grid_width,
-                game_state.grid_height,
-            )
-                .extend(0.0);
-            let location = (translation.truncate() / Vec2::splat(game_state.grid_step))
-                + Vec2::new(game_state.grid_width / 2.0, game_state.grid_height / 2.0);
-            if grid.0[location.x as usize][location.y as usize] == 0.0 {
-                commands
-                    .spawn_bundle(SpriteBundle {
-                        material: materials.add(particle_data.material.into()),
-                        transform: Transform::from_translation(translation),
-                        sprite: Sprite::new(Vec2::splat(game_state.grid_step)),
-                        ..Default::default()
-                    })
-                    .insert(particle_data)
-                    .insert(ParticleLocations {
-                        old_location: location,
-                        new_location: location,
-                    });
-                grid.0[location.x as usize][location.y as usize] = particle_data.density;
-            }
+        let translation = mouse_to_grid(
+            win,
+            game_state.grid_step,
+            game_state.grid_width,
+            game_state.grid_height,
+        )
+            .extend(0.0);
+        let location = (translation.truncate() / Vec2::splat(game_state.grid_step))
+            + Vec2::new(game_state.grid_width / 2.0, game_state.grid_height / 2.0);
+
+        if grid.0[location.x as usize][location.y as usize] == 0.0 {
+            commands
+                .spawn_bundle(SpriteBundle {
+                    material: materials.add(particle_data.material.into()),
+                    transform: Transform::from_translation(translation),
+                    sprite: Sprite::new(Vec2::splat(game_state.grid_step)),
+                    ..Default::default()
+                })
+                .insert(particle_data)
+                .insert(ParticleLocations {
+                    old_location: location,
+                    new_location: location,
+                });
+            grid.0[location.x as usize][location.y as usize] = particle_data.density;
         }
     }
 }
@@ -250,14 +265,15 @@ pub fn mouse_click_system(
 fn main() {
     App::build()
         .insert_resource(WindowDescriptor {
-            width: 900.,
-            height: 900.,
+            width: (GRID_WIDTH * GRID_STEP + 2 * GRID_STEP) as f32,
+            height: (GRID_HEIGHT * GRID_STEP + 2 * GRID_STEP) as f32,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
         .init_resource::<Grid>()
         .add_startup_system(startup_system.system())
         .add_system(mouse_click_system.system())
+        .add_system(keyboard_system.system())
         .add_stage_before(
             CoreStage::Update,
             MyStage::BeforeMove,
